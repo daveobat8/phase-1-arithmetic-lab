dcb93669b53efcc365c6947492686a96a834c1a1
David <dave.obat@student.moringashool.com>
2023-09-26 13:45:19 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1= 2
+let num2=31
+
+const multiply= num1*num2
+
+const random= 12
+
+let num3=4
+let num4=1
+const mod= num3/num4
+
+const max= (12,13,14,15,20)
+max.max
+
+// .max an operator
